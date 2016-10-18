b60f1aa0beffbec9f6f318a343d3b62acfbe2ee3
palong88 <patrick@plongmail.info>
2016-10-19 00:32:17 +0100

    test 2

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.enabled = true
-config.assets.precompile += Ckeditor.assets
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
 
 module Cvapp
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
